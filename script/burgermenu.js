0df29d72d8f6b13e741fbf1c09393465d4d3f8b4
ChristineDue <christine_due@yhaoo.dk>
2018-11-20 12:50:10 +0100

    Projekter oprettet

---
+++ script/burgermenu.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+//
+//function klik(){
+//			var burger = document.getElementById("navigation").value;
+//			
+//			document.getElementById("resultat").innerHTML = "Hej " + brugerensNavn
+//		}
+//
+//
+//$('.burger').click(function () {
+//  $(this).toggleClass('open');
+//});
